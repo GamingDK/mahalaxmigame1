0de155000d74db52a7bbb56c86794c0cdd67e3e3
dev <devanshsahu333@gamil.com>
2023-12-22 17:13:45 +0530

    push code

--- routes/andharBharRoute.js
+++ routes/andharBharRoute.js
@@ -1,16 +1,16 @@
 const router = require("express").Router();
-const {createMatch ,createBet ,createMatchCards, matchHistory} = require("../controllers/andharBharControlller.js");
+const { createMatch, createBet, createMatchCards, matchHistory } = require("../controllers/andharBharControlller.js");
 const handleValidationErrors = require("../middlewares/handleValidationErrors.js");
 const userAuthMiddleware = require("../middlewares/user/userAuthMiddleware.js");
 const { createBetValdiationRules } = require("../validations/andharBharValidationRules.js");
 
 //routes
 router.post("/create-match", createMatch);
-router.post("/create-match-cards",createMatchCards);
-router.get('/history',matchHistory);
+router.post("/create-match-cards", createMatchCards);
+router.get('/history', matchHistory);
 
 router.use(userAuthMiddleware);
-router.post("/create-bet", createBetValdiationRules , handleValidationErrors , createBet);
+router.post("/create-bet", createBetValdiationRules, handleValidationErrors, createBet);
 
 
 module.exports = router;
